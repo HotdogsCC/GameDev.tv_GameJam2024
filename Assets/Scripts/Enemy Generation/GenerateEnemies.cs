528b0a9de25a2c53c801d8f64fbbd6a06ae57fa4
Charlie Campbell <chacha9669@hotmail.com>
2024-05-28 10:41:32 +1000

    Enemy moves toward centre

--- Assets/Scripts/Enemy Generation/GenerateEnemies.cs
+++ Assets/Scripts/Enemy Generation/GenerateEnemies.cs
@@ -5,14 +5,10 @@ using UnityEngine;
 public class GenerateEnemies : MonoBehaviour
 {
     [SerializeField] private GameObject theEnemy;
-    [SerializeField] private int xPos;
-    [SerializeField] private int zPos;
     [SerializeField] private int enemyCount;
 
     [SerializeField] private float radius = 30f;
     [SerializeField] Transform origin;
-
-    [SerializeField] private BoxCollider box;
     
     void Start()
     {
@@ -24,35 +20,16 @@ public class GenerateEnemies : MonoBehaviour
         int i = 0;
         while (i < enemyCount)
         {
-            xPos = Random.Range(167, 190);
-            zPos = Random.Range(61, 123);
-            Instantiate(theEnemy, new Vector3(xPos, 1, zPos), Quaternion.identity);
+            float angle = Random.Range(0, 2 * Mathf.PI);
+            float xComponent = Mathf.Cos(angle) * radius;
+            float zComponent = Mathf.Sin(angle) * radius;
+
+            Vector3 position = new Vector3(origin.position.x + xComponent, 1, origin.position.z + zComponent);
+
+            Instantiate(theEnemy, position, Quaternion.identity);
             yield return new WaitForSeconds(0.1f);
             i += 1;
         }
-    }
-
-    IEnumerator EnemyDropBox()
-    {
-        while (enemyCount < 50)
-        {
-            //xPos = box.bounds.extends.x
-            //zPos = box.bounds.extends.y
-            Instantiate(theEnemy, new Vector3(xPos, 1, zPos), Quaternion.identity);
-            yield return new WaitForSeconds(0.1f);
-            enemyCount += 1;
-        }
-    }
-
-    private void Update()
-    {
-        float angle = Random.Range(0, 2 * Mathf.PI);
-        float xComponent = Mathf.Cos(angle) * radius;
-        float zComponent = Mathf.Sin(angle) * radius;
-
-        Vector3 position = new Vector3(origin.position.x + xComponent, 1, origin.position.z + zComponent);
-
-        Instantiate(theEnemy, position, Quaternion.identity);
     }
 
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,26 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Enemy : MonoBehaviour
 {
     [Header("Variables")]
     [SerializeField] private int health = 1;
     [SerializeField] private int money = 1;
+    [SerializeField] private float moveSpeed = 5;
 
     [Header("Object references")]
     [SerializeField] private GameObject deathVX;
+    [SerializeField] private GameObject origin;
+    private Rigidbody rb;
+    private Vector3 direction;
+    private Vector2 playerVector2;
+    private Vector2 originVector2;
+    private Vector2 playerToOriginVector2;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        origin = GameObject.FindGameObjectWithTag("origin");
+
+        playerVector2 = new Vector2(transform.position.x, transform.position.z);
+        originVector2 = new Vector2(origin.transform.position.x, origin.transform.position.z);
+        playerToOriginVector2 = originVector2 - playerVector2;
+        direction = new Vector3(playerToOriginVector2.x, 0, playerToOriginVector2.y);
+        direction = Vector3.Normalize(direction);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = direction * moveSpeed;
     }
 
     public void DamageTaken(int damageTaken)
@@ -39,4 +54,5 @@ public class Enemy : MonoBehaviour
         Instantiate(deathVX, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
+
 }
