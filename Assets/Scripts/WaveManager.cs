d957f030bd8ef0f6bbe5d3d0b6af8e71f1460f89
Charlie Campbell <chacha9669@hotmail.com>
2024-06-03 11:41:16 +1000

    Wave waiting + alerts

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
+using TMPro;
 
 public class WaveManager : MonoBehaviour
 {
@@ -9,10 +10,12 @@ public class WaveManager : MonoBehaviour
     private int currentWave = 1;
     public bool isWaveSpawning = true;
     public List<GenerateEnemies> waves;
+    [SerializeField] private TextMeshProUGUI waveText;
 
     private void Start()
     {
         waves[currentWave - 1].gameObject.SetActive(true);
+        StartCoroutine(DisplayText("Wave " + currentWave.ToString()));
     }
 
     private void Update()
@@ -20,10 +23,27 @@ public class WaveManager : MonoBehaviour
         //When the wave is over
         if(!isWaveSpawning && currentAmountOfEnemies <= 0)
         {
+            StartCoroutine(DisplayText("A wave is coming soon..."));
             currentAmountOfEnemies = 0;
             isWaveSpawning = true;
             currentWave++;
-            waves[currentWave - 1].gameObject.SetActive(true);
+            StartCoroutine(SpawnEnemies());
         }
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        yield return new WaitForSeconds(15);
+        currentAmountOfEnemies = 0;
+        isWaveSpawning = true;
+        waves[currentWave - 1].gameObject.SetActive(true);
+        StartCoroutine(DisplayText("Wave " + currentWave.ToString()));
+    }
+
+    private IEnumerator DisplayText(string text)
+    {
+        waveText.text = text;
+        yield return new WaitForSeconds(5);
+        waveText.text = null;
     }
 }
